45f3561706729c9bf0798955c6a613dcbdc5740c
Danil Samsonov <thrashdev@protonmail.com>
2024-07-08 13:41:53 +0500

    improved tests for leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,13 +1,9 @@
 from types import NotImplementedType
 
-class HTMLTag():
-    def __init__(self, tag) -> None:
-        self.opening = f"<{tag}>"
-        self.closing = f"</{tag}>"
 
 class HTMLNode():
     def __init__(self, tag=None, value=None, children=None, props:dict=None) -> None:
-        self.tag = HTMLTag(tag)
+        self.tag = tag
         self.value = value
         self.children = children
         self.props = props
@@ -17,13 +13,23 @@ class HTMLNode():
 
     def props_to_html(self):
         html = ""
+        if self.props == {}: return html
+        if self.props is None: return html
         for k,v in self.props.items():
-            html += f'{k}="{v}" '
+            html += f' {k}="{v}"'
         return html
 
     def __repr__(self) -> str:
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props}"
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props: dict = None) -> None:
+    def __init__(self, tag, value:str, props: dict = None) -> None:
+        assert(value is not None)
         super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        return f"<{self.tag}{self.props_to_html()}> {self.value} </{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props: dict = None) -> None:
+        super().__init__(tag,  children, props)
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,11 +1,27 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
-        props = {}
-        props["a"] = "www.google.com"
-        node = HTMLNode("p", "this is text of the paragraph", None, props )
-        expected_props = 'a = "www.google.com"'
+        props = {"href" : "https://www.google.com"}
+        node = HTMLNode("a", "this is text of the paragraph", None, props )
+        expected_props = ' href="https://www.google.com"'
         self.assertEqual(node.props_to_html(), expected_props)
+
+class TestLeafNode(unittest.TestCase):
+    def test_paragraph(self):
+        node = LeafNode("p", "This is a paragraph of text")
+        target_html = "<p> This is a paragraph of text </p>"
+        self.assertEqual(node.to_html(), target_html)
+
+
+    def test_anchor(self):
+        node = LeafNode("a", "Click me!", {"href" : "https://www.google.com"})
+        target_html = '<a href="https://www.google.com"> Click me! </a>'
+        print(node)
+        print(target_html)
+        self.assertEqual(node.to_html(), target_html)
+        
+if __name__ == '__main__':
+    unittest.main()
